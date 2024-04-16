152b4c9a14ae9c6d03d6075be6b182699903eb94
Rodrigo Cañiza <167246602+canizarodrigo@users.noreply.github.com>
2024-04-16 16:52:22 -0300

    subiendo el archivo py

---
+++ code.py
@@ -0,0 +1,9 @@
+nombre = "rodrigo"
+apellido = "cañiza"
+
+<<<<<<< HEAD
+print("hola "+ nombre + " " + apellido + "cómo estás?")
+=======
+saludo= "hola "+ nombre + " " + apellido
+print(saludo)
+>>>>>>> conflict-branch
